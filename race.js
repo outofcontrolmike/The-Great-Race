221d43ac3a3025526c99184dc59b92181f525b77
Michael Wilson <mw0415436@otc.edu>
2018-10-10 15:27:29 -0500

    created function for image changing

--- race.js
+++ race.js
@@ -1,4 +1,14 @@
 
-function changeStar() {
-    document.getElementById('star').src="on.png";
+// Three events that will happen
+var stopLight = document.getElementById('star');
+var marioRun = document.getElementById('mario');
+var yoshiRun = document.getElementById('yoshi');
+
+stopLight.addEventListener("click", startRace);
+myTimer = setInterval(startRace, 3000);
+
+function startRace(){
+     stopLight = document.getElementById('star').src = "on.png";
+     marioRun = document.getElementById('mario').src = "mariorun.png";
+     yoshiRun = document.getElementById('yoshi').src = "yoshirun.png";
 }
