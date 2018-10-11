a9fb257e4ee1d778db567b853925c5c685877e55
Michael Wilson <mw0415436@otc.edu>
2018-10-10 21:32:12 -0500

    Need to get racers moving

--- race.js
+++ race.js
@@ -4,11 +4,26 @@ var stopLight = document.getElementById('star');
 var marioRun = document.getElementById('mario');
 var yoshiRun = document.getElementById('yoshi');
 
+// function that prepares race, swaps images
+
 stopLight.addEventListener("click", startRace);
-myTimer = setInterval(startRace, 3000);
 
 function startRace(){
      stopLight = document.getElementById('star').src = "on.png";
      marioRun = document.getElementById('mario').src = "mariorun.png";
      yoshiRun = document.getElementById('yoshi').src = "yoshirun.png";
+     
 }
+
+// function that starts race
+
+var moveMario = document.getElementById('mario');
+var moveYoshi = document.getElementById('yoshi');
+var moveLight = document.getElementById('star');
+
+moveLight.addEventListener("click", moveImage);
+myTimer = setInterval(moveImage, 3000);
+
+function moveImage() {
+    document.getElementById('mario').style.right;
+}   document.getElementById('yoshi').style.right;
