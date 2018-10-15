64809294fa33a6102b6c33de3e1cf471569ec76e
WILSON <mw0415436@otc.edu>
2018-10-15 15:47:01 -0500

    Need to make a stop point

--- race.js
+++ race.js
@@ -12,15 +12,38 @@ function startRace(){
      stopLight = document.getElementById('star').src = "on.png";
      marioRun = document.getElementById('mario').src = "mariorun.png";
      yoshiRun = document.getElementById('yoshi').src = "yoshirun.png";
-     
+     document.getElementById('mario').style.left = 0;
+    document.getElementById('yoshi').style.left = 0;
 
 // function that starts race
 
-document.getElementById('mario').style.left = 0;
-document.getElementById('yoshi').style.left = 0;
+var myTime = setInterval(startRace, 10);
+var marioRacer = document.getElementById('mario')
+var yoshiRacer = document.getElementById('yoshi');
 
-setInterval(function(){
+function startRace() {
     var rand = Math.round(Math.random() * 20);
-    document.getElementById('mario').style.left = (parseInt(document.getElementById('mario').style.left) + rand) + 'px';
-    document.getElementById('yoshi').style.left = (parseInt(document.getElementById('yoshi').style.left) + rand) + 'px';
-}, 500)}
+    var rand2 = Math.round(Math.random() *20);
+   var marioRacer = document.getElementById('mario').style.left = (parseInt(document.getElementById('mario').style.left) + rand2) + 'px';
+   var yoshiRacer = document.getElementById('yoshi').style.left = (parseInt(document.getElementById('yoshi').style.left) + rand) + 'px';
+
+   //testing purposes
+   console.log(rand2);
+   console.log(marioRacer);
+
+
+   if (marioRacer >= 1000) {
+    alert("Mario is number 1!");
+    stopRace();
+
+}else if (yoshiRacer >= 1000) {
+    alert("Yoshi wins the race!");
+}   stopRace2();
+
+
+function myStopFunction() {
+    clearInterval(myTime)
+}
+}}
+
+// Function that preprares load up screen by clicking winner
