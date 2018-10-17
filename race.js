361b35077f46e1164e3090ea8d49c61754fbd67a
Michael Wilson <mw0415436@otc.edu>
2018-10-16 20:52:30 -0500

    still need to make racers stop

--- race.js
+++ race.js
@@ -1,12 +1,14 @@
 
-// Three events that will happen
-var stopLight = document.getElementById('star');
-var marioRun = document.getElementById('mario');
-var yoshiRun = document.getElementById('yoshi');
+// Three events that will happen/ declaring variables
+    let stopLight = document.getElementById('star');
+    let marioRun = document.getElementById('mario');
+    let yoshiRun = document.getElementById('yoshi');
 
 // function that prepares race, swaps images
 
+
 stopLight.addEventListener("click", startRace);
+
 
 function startRace(){
 
@@ -18,37 +20,46 @@ function startRace(){
 
 // function that starts race
 
-var myTime = setInterval(startRace, 10);
-var marioRacer = marioRun;
-var yoshiRacer = yoshiRun;
-
+    var myTime = setInterval(startRace, 20);
+   
+// 
 function startRace() {
     var rand = Math.round(Math.random() * 20);
     var rand2 = Math.round(Math.random() *20);
-   var marioRacer = document.getElementById('mario').style.left = (parseInt(document.getElementById('mario').style.left) + rand2) + 'px';
-   var yoshiRacer = document.getElementById('yoshi').style.left = (parseInt(document.getElementById('yoshi').style.left) + rand) + 'px';
+    let marioRacer = document.getElementById('mario').style.left = (parseInt(document.getElementById('mario').style.left) + rand2) + 'px';
+    let yoshiRacer = document.getElementById('yoshi').style.left = (parseInt(document.getElementById('yoshi').style.left) + rand) + 'px';
 
    //testing purposes
    console.log(rand2);
    console.log(marioRacer);
 
+   //if statment to stop racer at finsh point
 
-   if (marioRacer >= 1000) {
+
+    if (marioRacer >= 1200) {
     alert("Mario is number 1!");
-    stopRace();
+    myStopFunction();
 
-}else if (yoshiRacer >= 1000) {
+}   else if (yoshiRacer >= 1200) {
     alert("Yoshi wins the race!");
-}   stopRace2();
+}   myStopFunction();
 
 
-function myStopFunction() {
+
+/*function myStopFunction() {
     clearInterval(myTime)
-}
+*/ 
+
 }}
 
 // Function that preprares load up screen by clicking winner
+let marioStand = document.getElementById('mario').src="mario.png";
+let yoshiStand = document.getElementById('yoshi').src="yoshi.png";
+let goLoad =     document.getElementById('star').src="off.png";
 
+function resetPage() {
+    document.getElementById()
+}
 
 // Need to figure out how to make characters stop at a certain pixel
 
